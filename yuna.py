7d4e5c7554326f622f999bf00c1e1b7e1b2a992f
iWeeti <maaninkaveeti@gmail.com>
2018-09-22 00:49:08 +0300

    added error and guild logging

--- yuna.py
+++ yuna.py
@@ -63,6 +63,64 @@ class Yuna(commands.AutoShardedBot):
 		await self.bot.change_presence(activity=discord.Activity(name='y?help | UwU', type=discord.ActivityType.listening))
 		self.loop.create_task(self.avatar_queue())
 
+	@property
+    def logging_ch(self):
+        hook = self.bot.get_channel(492797168005152778)
+        return hook
+
+	async def send_guild_stats(self, e, guild):
+        e.add_field(name='Name', value=guild.name)
+        e.add_field(name='ID', value=guild.id)
+        e.add_field(name='Owner', value=f'{guild.owner} (ID: {guild.owner.id})')
+
+        bots = sum(m.bot for m in guild.members)
+        total = guild.member_count
+        online = sum(m.status is discord.Status.online for m in guild.members)
+        e.add_field(name='Members', value=str(total))
+        e.add_field(name='Bots', value=f'{bots} ({bots/total:.2%})')
+        e.add_field(name='Online', value=f'{online} ({online/total:.2%})')
+
+        if guild.icon:
+            e.set_thumbnail(url=guild.icon_url)
+
+        if guild.me:
+            e.timestamp = guild.me.joined_at
+
+        await self.logging_ch.send(embed=e)
+
+    async def on_guild_join(self, guild):
+        e = discord.Embed(colour=0x53dda4, title='New Guild') # green colour
+        await self.bot.pool.execute(f'insert into guilds values ({guild.id})')
+        await self.send_guild_stats(e, guild)
+
+    async def on_guild_remove(self, guild):
+        e = discord.Embed(colour=0xdd5f53, title='Left Guild') # red colour
+        await self.bot.pool.execute(f'delete from guilds where id={guild.id}')
+        await self.send_guild_stats(e, guild)
+
+    async def on_command_error(self, ctx, error):
+        ignored = (commands.NoPrivateMessage, commands.DisabledCommand, commands.CheckFailure,
+                   commands.CommandNotFound, commands.UserInputError, discord.Forbidden, commands.CommandOnCooldown)
+        error = getattr(error, 'original', error)
+
+        if isinstance(error, ignored):
+            return
+
+        e = discord.Embed(title='Command Error', colour=0xcc3366)
+        e.add_field(name='Name', value=ctx.command.qualified_name)
+        e.add_field(name='Author', value=f'{ctx.author} (ID: {ctx.author.id})')
+
+        fmt = f'Channel: {ctx.channel} (ID: {ctx.channel.id})'
+        if ctx.guild:
+            fmt = f'{fmt}\nGuild: {ctx.guild} (ID: {ctx.guild.id})'
+
+        e.add_field(name='Location', value=fmt, inline=False)
+
+        exc = ''.join(traceback.format_exception(type(error), error, error.__traceback__, chain=False))
+        e.description = f'```py\n{exc}\n```'
+        e.timestamp = datetime.datetime.utcnow()
+        await self.logging_ch.send(embed=e)
+
 	def get_guild_prefixes(self, guild, *, local_inject=get_prefix):
 	    """Gets the guild prefixes."""
 	    proxy_msg = discord.Object(id=None)
