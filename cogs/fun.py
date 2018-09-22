1df672bd69e6e718a328a1f5cfc61ab9ab661743
☾lukee <36803432+paixlukee@users.noreply.github.com>
2018-09-21 21:36:18 -0400

    Fix ASCII

--- cogs/fun.py
+++ cogs/fun.py
@@ -11,9 +11,8 @@ class Fun():
     async def ascii(self, ctx, *, text):
         text = text.replace(" ", "+")
         r = requests.get(f"http://artii.herokuapp.com/make?text={text}")
-        rc = r.content
-        rc = rc.replace("\n", "\n")
-        await ctx.send(f"```{rc}```")
+        rc = r.replace("\n", "\n")
+        await ctx.send(f"```{rc.content}```")
       
     
         
