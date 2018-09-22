08405c6cad1e396f0b111bb2f58adfd8e04ab288
☾lukee <36803432+paixlukee@users.noreply.github.com>
2018-09-21 21:49:36 -0400

    Fix ASCII

--- cogs/fun.py
+++ cogs/fun.py
@@ -9,15 +9,16 @@ class Fun():
         
     @commands.command()
     async def ascii(self, ctx, *, text):
+        """Convert your text to ASCII"""
         text = text.replace(' ', '\n')
         
         if not text:
-            await ctx.send(f"{ctx.tick(False)} You need to specify what anime you want to search for!")
+            await ctx.send(f"{ctx.tick(False)} You need to specify the text you want to convert!")
             
         _fig = figlet_format(text)
         
         if len(_fig) > 1700:
-            await ctx.send(f"{ctx.tick(False} The message is too long!")
+            await ctx.send(f"{ctx.tick(False)} The message is too long!")
         await ctx.send(f"Here you go!\n```{_fig}```")
       
     
