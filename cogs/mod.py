ea70f800a391ed21ffcc03aef4a0ca5cd06ff364
iWeeti <maaninkaveeti@gmail.com>
2018-09-28 23:42:33 +0300

    trying to fix purge

--- cogs/mod.py
+++ cogs/mod.py
@@ -79,15 +79,15 @@ class Mod:
 		to_send = '\n'.join(messages)
 
 		if len(to_send) > 2000:
-		    await ctx.send(f'{ctx.tick(True)} Removed {deleted_messages} messages.', delete_after=10)
-		    await asyncio.sleep(10)
+			await ctx.send(f'{ctx.tick(True)} Removed {deleted_messages} messages.', delete_after=10)
+			await asyncio.sleep(10)
 			if ctx.message:
-			    await ctx.message.delete()
+				await ctx.message.delete()
 		else:
-		    await ctx.send(to_send, delete_after=10)
-		    await asyncio.sleep(10)
+			await ctx.send(to_send, delete_after=10)
+			await asyncio.sleep(10)
 			if ctx.message:
-			    await ctx.message.delete()
+				await ctx.message.delete()
 
 	@commands.group()
 	@checks.is_mod()
