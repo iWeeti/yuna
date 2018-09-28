21fb18e1d1789ac4a8ac57df2c6fef64cde64f3b
iWeeti <maaninkaveeti@gmail.com>
2018-09-28 22:54:48 +0300

    added ban

--- cogs/mod.py
+++ cogs/mod.py
@@ -18,7 +18,7 @@ class Mod:
 
 	@commands.command()
 	@checks.is_mod()
-	async def kick(self, ctx, members:commands.Greedy[discord.Member]=None, *, reason: Reason=None):
+	async def kick(self, ctx, members:commands.Greedy[discord.Member, discord.User]=None, *, reason: Reason=None):
 		if not members:
 			return await ctx.send(f'{ctx.tick(False)} You need to specify at least one member to kick.')
 
@@ -32,6 +32,23 @@ class Mod:
 				return await ctx.send(f'{ctx.tick(False)} Failed to kick, I need kick members permissions to do this.')
 
 		await ctx.send(f'{ctx.tick(True)} Kicked {", ".join(kicked)}.')
+
+	@commands.command()
+	@checks.is_mod()
+	async def ban(self, ctx, members:commands.Greedy[discord.Member]=None, *, reason: Reason=None):
+		if not members:
+			return await ctx.send(f'{ctx.tick(False)} You need to specify at least one member to ban.')
+
+		banned = []
+
+		for member in members:
+			try:
+				await ctx.guild.ban(member, reason=reason)
+				banned.append(member)
+			except discord.Forbidden:
+				return await ctx.send(f'{ctx.tick(False)} I need ban members permissions to run this command.')
+
+		await ctx.send(f'{ctx.tick(True)} Banned {", ".join(banned)}.')
 
 def setup(bot):
 	bot.add_cog(Mod(bot))
