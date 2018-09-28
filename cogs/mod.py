ac427117bf07e9e412ff91d541c9959e92d137ba
iWeeti <maaninkaveeti@gmail.com>
2018-09-28 23:19:46 +0300

    trying to add purge

--- cogs/mod.py
+++ cogs/mod.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 import discord
 from .utils import checks
+from collections import Counter
 
 
 class Reason(commands.Converter):
@@ -49,6 +50,74 @@ class Mod:
 				return await ctx.send(f'{ctx.tick(False)} I need ban members permissions to run this command.')
 
 		await ctx.send(f'{ctx.tick(True)} Banned {", ".join(banned)}.')
+
+	async def remove_messages(self, ctx, limit:int=100, pred, before=None, after=None):
+		if limit > 2000:
+			return await ctx.send(f'{ctx.tick(False)} Invalid limit. {limit}/2000')
+
+		before = discord.Object(id=before) or ctx.message
+
+		if after is not None:
+			after = discord.Object(id=after)
+
+		try:
+			deleted_messages = await ctx.channel.purge(limit=limit, before=before, after=after, predicate=pred)
+		except discord.Forbidden:
+			return await ctx.send(f'{ctx.tick(False)} I do not have delete messages permissions.')
+		except discord.HTTPException as e:
+			return await ctx.send(f'Oops... {e}, maybe try a smaller limit.')
+
+		deleted_counter = Counter(m.author.display_name for m in deleted_messages)
+        deleted_messages = len(deleted_messages)
+        messages = [f'{deleted_messages} message{" was" if deleted_messages == 1 else "s were"} purged.']
+        if deleted_messages:
+            messages.append('')
+            deleted_counter = sorted(deleted_counter.items(), key=lambda t: t[1], reverse=True)
+            messages.extend(f'**{name}**: {count}' for name, count in deleted_counter)
+
+        to_send = '\n'.join(deleted_messages)
+
+        if len(to_send) > 2000:
+            await ctx.send(f'{ctx.tick(True)} Successfully purged {deleted_messages} messages.', delete_after=10)
+            await asyncio.sleep(10)
+            await ctx.message.delete()
+        else:
+            await ctx.send(to_send, delete_after=10)
+            await asyncio.sleep(10)
+            await ctx.message.delete()
+
+	@commands.group()
+	@checks.is_mod()
+	async def purge(self, ctx):
+		if not ctx.invoked_subcommand:
+			return await ctx.show_help('purge')
+
+	@purge.command(name='all')
+	@checks.is_mod()
+	async def purge_all(self, ctx, limit:int=100):
+		await self.remove_messages(ctx, limit, lambda m: True)
+
+	@purge.command(name='embeds', aliases=['embed'])
+	@checks.is_mod()
+	async def purge_embeds(self, ctx, limit:int=100):
+		await self.remove_messages(ctx, limit, lambda m: len(m.embeds))
+
+	@purge.command(name='files', aliases=['file'])
+	@checks.is_mod()
+	async def purge_files(self, ctx, limit:int=100):
+		await self.remove_messages(ctx, limit, lambda m: len(m.attachments))
+
+	@purge.command(name='images', aliases=['image'])
+	@checks.is_mod()
+	async def purge_images(self, ctx, limit:int=100):
+		await self.remove_messages(ctx, limit, lambda m: len(e.embeds) or len(e.attachments))
+
+	@purge.command(name='user', aliases=['users'])
+	@checks.is_mod()
+	async def purge_user(self, ctx, members:commands.Greedy[discord.Member]=None, limit:int=100):
+		if not members:
+			return await ctx.send(f'{ctx.tick(False)} You need to specify at least one member to purge.')
+		await self.remove_messages(ctx, limit, lambda m: m.author.id == member.id for member in members)
 
 def setup(bot):
 	bot.add_cog(Mod(bot))
